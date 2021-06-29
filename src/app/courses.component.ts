b76d82a969d560002182eb8d6bc492cf4794233d
zubair-pc\zubair <szubair@gmail.com>
2021-06-29 16:50:13 +0500

    ngFor directive is used to show a list

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,9 +2,20 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'courses',
-    template:'<h2>{{"Title: " + title}}</h2>'
+    template:`
+    <h2>{{"Title: " + title}}</h2>
+    <ul>
+       <li>{{courses[0]}}</li>
+       <li>{{courses[1]}}</li>
+       <li>{{courses[2]}}</li>
+    </ul>
+    <ol>
+        <li *ngFor="let course of courses">{{course}}</li>
+    </ol>
+    `
 })
 export class CoursesComponent{
     title:string ="List of courses";
+    courses =["course1","course2","course3"];
 
 }
